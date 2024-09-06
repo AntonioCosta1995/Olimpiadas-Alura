dfbb687c63bf5b101b744e4afbf19e7d22fc0571
AntonioCosta1995 <antoniocosta157@hotmail.com>
2024-09-06 11:59:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados =[
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, conhecida como a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos de idade, ela já coleciona títulos e inspira milhões de jovens.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags : "skate fada sk8"
+    
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro. Sua técnica e determinação a tornaram um ícone do judô feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags : "judo judô judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira que fez história nos Jogos Olímpicos de Tóquio 2020 e Paris 2024, conquistando diversas medalhas. Sua versatilidade e força a colocaram entre as melhores ginastas do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "ginástica ginastica ouro"
+    }
+];
+
